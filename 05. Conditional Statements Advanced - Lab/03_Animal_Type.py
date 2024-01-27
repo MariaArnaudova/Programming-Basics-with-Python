6990538598194b1237113572c13fcc7bd506b357
MariaCode <m.p.arnaudova@gmail.com>
2024-01-27 10:56:57 +0200

    Added solution

---
+++ 05. Conditional Statements Advanced - Lab/03_Animal_Type.py
@@ -0,0 +1,11 @@
+animal = input()
+output = ''
+
+if animal == 'dog':
+    output = 'mammal'
+elif animal == "crocodile" or animal == "tortoise" or animal == "snake":
+    output = 'reptile'
+else :
+    output = 'unknown'
+
+print(output)
